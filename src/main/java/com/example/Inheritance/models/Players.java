688fe44b3998a4db35c133b8d67ca9cf26e1aa48
TanaySrivas <tanaysriofficial@gmail.com>
2024-04-29 19:13:45 +0530

    player model

--- src/main/java/com/example/Inheritance/models/Players.java
+++ src/main/java/com/example/Inheritance/models/Players.java
@@ -1,5 +1,17 @@
 package com.example.Inheritance.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.hibernate.id.uuid.UuidGenerator;
+
+import java.util.UUID;
+
 public class Players {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+    private String name;
+    private int age;
 
 }
